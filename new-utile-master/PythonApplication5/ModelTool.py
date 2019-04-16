85bb799486a8c55da241ff242b75c377d8721edf
wenjunLi965146983 <965146983@qq.com>
2019-04-16 11:45:47 +0800

    add feature map tool

--- new-utile-master/PythonApplication5/ModelTool.py
+++ new-utile-master/PythonApplication5/ModelTool.py
@@ -7,7 +7,9 @@ Edit by LWJ 2019-4-10
 '''
 
 from functools import reduce
-
+from matplotlib import pyplot as plt
+from keras import models
+import numpy as np
 
 def compose(*funcs):
     """
@@ -23,16 +25,42 @@ def compose(*funcs):
         raise ValueError('Composition of empty sequence not supported.')
 
 
-def feature_map(model,images_per_row = 16):
+def feature_map(model,image,layers_slice,images_per_row = 16):
+    '''
+    Input a model and a image, to show the activation of each layer
+    model: input a model
+    image: input a image by normalization
+    layers_slice:input a list [star,end],if(end==0) end len(layers)-1
+
+    '''
+   
+
+  
+
+    def get_layer(number):
+        result=number if number>0 else len(model.layers)+number
+        return result
+
+
+
+
+    
+    layer_outputs = [layer.output for layer in model.layers[get_layer(layers_slice[0]):get_layer(layers_slice[1])]] 
+    
+    activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
+    activations = activation_model.predict(image) 
+
 
     layer_names = []
-
-    for layer in layers:     
+    
+    for layer in layer_outputs:     
         layer_names.append(layer.name) #层的名称，这样你可以将这些名称画到图中
  
      
  
     for layer_name, layer_activation in zip(layer_names, activations):     #显示特征图  
+
+        if len(layer_activation.shape)>2:
            n_features = layer_activation.shape[-1]   #特征图中的特征个数
  
            size = layer_activation.shape[1]   #特征图的形状为 (1, size, size, n_features)
@@ -43,9 +71,7 @@ def feature_map(model,images_per_row = 16):
  
            for col in range(n_cols):           #将每个过滤器平铺到 一个大的水平网格中
                for row in range(images_per_row):             
-                   channel_image = layer_activation[0,
-                                                     :, :,                                              
-                                                     col * images_per_row + row]             
+                   channel_image = layer_activation[...,col * images_per_row + row]             
                    channel_image -= channel_image.mean()           #对特征进行后 处理，使其看 起来更美观    
                    channel_image /= channel_image.std()             
                    channel_image *= 64             
@@ -55,10 +81,15 @@ def feature_map(model,images_per_row = 16):
                                 row * size : (row + 1) * size] = channel_image 
  
 
-    scale = 1. / size     
-    plt.figure(figsize=(scale * display_grid.shape[1],                         
+           scale = 1. / size     
+           plt.figure(figsize=(scale * display_grid.shape[1],                         
                         scale * display_grid.shape[0]))     
-    plt.title(layer_name)     
-    plt.grid(False)     
-    plt.imshow(display_grid, aspect='auto', cmap='viridis')
+           plt.title(layer_name)     
+           plt.grid(False)     
+           plt.imshow(display_grid, aspect='auto', cmap='viridis')
+           plt.show()
+        else:
+            result =layer_activation
+            print(layer_name)
+            np.savetxt('feature_map',result)
 
--- new-utile-master/PythonApplication5/ModelToolTest.py
+++ new-utile-master/PythonApplication5/ModelToolTest.py
@@ -1,11 +1,16 @@
 from keras.applications import vgg16
 from keras import models
 from keras import layers
-
 import keras.backend as K
 import tensorflow as tf
 import numpy as np
-from ModelTollTest import feature_map
+from matplotlib import pyplot as plt
+
+
+
+import ModelTool as t
+import utile as lwj
+
 
 '''
 model 
@@ -17,19 +22,35 @@ def test():
     
 
     K.clear_session() # get a new session
-    model=vgg16.VGG16(include_top=False,weights='imagenet',input_shape=(448,448,3))
+    model=vgg16.VGG16(include_top=True,weights='imagenet',input_shape=(224,224,3))
 
     for layer in model.layers:
         layer.trainable=False
 
+    model.summary()
 
 
-    feature_map()
+    generater=lwj.datagenerater("E:/VOC2012/JPEGImages/","E:/VOC2012/Annotations/",batch_size=1,image_size=(224,224))
+    datagenerater=generater.get_train_data()
+    
+    keep_run=True
+    while keep_run:
+        image,label=next(datagenerater)
+        tt=np.reshape(image,(224,224,3))
+        plt.imshow(tt)
+        plt.show()
+        image=image/255
+        t.feature_map(model,image,[-1,0])
+        a=input('input:')
+        if a=='l':
+            keep_run=False
+
+    
+   
+  
 
 
 
 
-
-
-if __name__== 'main':
+if __name__== '__main__':
     test()
